fb83fe0cd2ade77885a8ad348c2550142b293314
Harry Billinis <31311538+Tiltorito@users.noreply.github.com>
2018-02-01 12:38:59 +0000

    Added comments on Logger class

--- src/mqtt/utilities/logger/Logger.java
+++ src/mqtt/utilities/logger/Logger.java
@@ -24,42 +24,110 @@ public class Logger implements BaseLogger {
         this.tag = tag;
     }
 
+    /**
+     * Printing in the console and writing the message in the error log file.
+     * @param msg the message to be written
+     * @return true if the message has been written successfully, false if something went wrong
+     */
     @Override
     public boolean e(String msg) {
         return logger.e(addTag(msg));
     }
 
+    /**
+     * Printing in the console and writing the message in the debug log file.
+     * @param msg the message to be written
+     * @return true if the message has been written successfully, false if something went wrong
+     */
     @Override
     public boolean d(String msg) {
         return logger.d(addTag(msg));
     }
 
+    /**
+     * Printing in the console and writing the message in the info log file.
+     * @param msg the message to be written
+     * @return true if the message has been written successfully, false if something went wrong
+     */
     @Override
     public boolean i(String msg) {
         return logger.i(addTag(msg));
     }
 
+    /**
+     * Printing in the console and writing the message in the warning log file.
+     * @param msg the message to be written
+     * @return true if the message has been written successfully, false if something went wrong
+     */
     @Override
     public boolean w(String msg) {
         return logger.w(addTag(msg));
     }
 
-
+    /**
+     * Executes a block of code while writing comments anywhere around it, ie,
+     * Starting loading files
+     * Completed loading files OR
+     * Failed loading files if an exception is be thrown in the block which passed as argument.
+     * The logs are error logs.
+     * @param description the description message that will be used for the logs
+     * @param block the block to be executed
+     * @param <T> the return type of the block
+     * @return the return value of the block
+     * @throws BlockFailedException if the block throws an exception
+     */
     public <T> T withErrorLogs(String description, BlockWithReturn<T> block) throws BlockFailedException {
         return withLogging(this::e, description, block);
     }
 
+    /**
+     * Executes a block of code while writing comments anywhere around it, ie,
+     * Starting loading files
+     * Completed loading files OR
+     * Failed loading files if an exception is be thrown in the block which passed as argument.
+     * The logs are debug logs.
+     * @param description the description message that will be used for the logs
+     * @param block the block to be executed
+     * @param <T> the return type of the block
+     * @return the return value of the block
+     * @throws BlockFailedException if the block throws an exception
+     */
     public <T> T withDebugLogs(String description, BlockWithReturn<T> block) throws BlockFailedException {
         return withLogging(this::d, description, block);
     }
 
+    /**
+     * Executes a block of code while writing comments anywhere around it, ie,
+     * Starting loading files
+     * Completed loading files OR
+     * Failed loading files if an exception is be thrown in the block which passed as argument.
+     * The logs are info logs.
+     * @param description the description message that will be used for the logs
+     * @param block the block to be executed
+     * @param <T> the return type of the block
+     * @return the return value of the block
+     * @throws BlockFailedException if the block throws an exception
+     */
     public <T> T withInfoLogs(String description, BlockWithReturn<T> block) throws BlockFailedException {
         return withLogging(this::i, description, block);
     }
 
+    /**
+     * Executes a block of code while writing comments anywhere around it, ie,
+     * Starting loading files
+     * Completed loading files OR
+     * Failed loading files if an exception is be thrown in the block which passed as argument.
+     * The logs are warning logs.
+     * @param description the description message that will be used for the logs
+     * @param block the block to be executed
+     * @param <T> the return type of the block
+     * @return the return value of the block
+     * @throws BlockFailedException if the block throws an exception
+     */
     public <T> T withWarningLogs(String description, BlockWithReturn<T> block) throws BlockFailedException {
         return withLogging(this::w, description, block);
     }
+
 
     private <T> T withLogging(Consumer<String> method, String description, BlockWithReturn<T> block) throws BlockFailedException {
         method.accept("Starting " + description);
@@ -78,6 +146,10 @@ public class Logger implements BaseLogger {
         return res;
     }
 
+    /**
+     * Closing all the log files that is used by the logger.
+     * @return true if everything have been closed successfully, false if at least one file couldn't close or threw an exception
+     */
     @Override
     public boolean close() {
         return logger.close();
